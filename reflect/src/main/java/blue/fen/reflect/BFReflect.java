5bc0982aa7f89f6488a9157b314b385e5c12b2d4
xujianfen <xujianfen@taplex.games>
2023-04-29 12:48:46 +0800

    feat - 支持反射hide

--- reflect/src/main/java/blue/fen/reflect/BFReflect.java
+++ reflect/src/main/java/blue/fen/reflect/BFReflect.java
@@ -1,5 +1,9 @@
 package blue.fen.reflect;
 
+import android.os.Build;
+
+import java.lang.reflect.Method;
+
 import blue.fen.reflect.core.expose.BFClass;
 import blue.fen.reflect.core.expose.BFField;
 import blue.fen.reflect.core.expose.BFMethod;
@@ -15,6 +19,30 @@ import blue.fen.reflect.core.impl.method.BFReflectMethod;
  * 描述：用于反射的工具类
  */
 public class BFReflect {
+    static {
+        //在高版本SDK中，很多方法都被hidden，所以需要绕过hidden对系统代码的屏蔽
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            try {
+                Method forName = Class.class.getDeclaredMethod("forName", String.class);
+                Method getDeclaredMethod = Class.class.getDeclaredMethod("getDeclaredMethod",
+                        String.class, Class[].class);
+
+                Class<?> vmRuntimeClass = (Class<?>) forName.invoke(null, "dalvik.system.VMRuntime");
+
+                Method getRuntime = (Method) getDeclaredMethod.invoke(vmRuntimeClass, "getRuntime", null);
+                Method setHiddenApiExemptions = (Method) getDeclaredMethod.invoke(vmRuntimeClass,
+                        "setHiddenApiExemptions", new Class[]{String[].class});
+
+                if (getRuntime != null && setHiddenApiExemptions != null) {
+                    Object sVmRuntime = getRuntime.invoke(null);
+                    setHiddenApiExemptions.invoke(sVmRuntime, new Object[]{new String[]{"L"}});
+                }
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public final static BFField FIELD;
 
     public final static BFMethod METHOD;
